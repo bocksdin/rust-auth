133cccd8d964bcafb3f6c45dac3548d1ab2dbf36
Rory Letteney <rory.letteney@gmail.com>
2022-10-30 15:42:19 -0500

    Complete code

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,45 @@ use jwt::VerifyWithKey;
 use serde::{Deserialize, Serialize};
 use sha2::Sha256;
 
+mod services;
+use services::{basic_auth, create_article, create_user};
+
 pub struct AppState {
     db: Pool<Postgres>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct TokenClaims {
+    id: i32,
+}
+
+async fn validator(
+    req: ServiceRequest,
+    credentials: BearerAuth,
+) -> Result<ServiceRequest, (Error, ServiceRequest)> {
+    let jwt_secret: String = std::env::var("JWT_SECRET").expect("JWT_SECRET must be set!");
+    let key: Hmac<Sha256> = Hmac::new_from_slice(jwt_secret.as_bytes()).unwrap();
+    let token_string = credentials.token();
+
+    let claims: Result<TokenClaims, &str> = token_string
+        .verify_with_key(&key)
+        .map_err(|_| "Invalid token");
+
+    match claims {
+        Ok(value) => {
+            req.extensions_mut().insert(value);
+            Ok(req)
+        }
+        Err(_) => {
+            let config = req
+                .app_data::<bearer::Config>()
+                .cloned()
+                .unwrap_or_default()
+                .scope("");
+
+            Err((AuthenticationError::from(config).into(), req))
+        }
+    }
 }
 
 #[actix_web::main]
@@ -33,8 +70,19 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Error building a connection pool");
 
-    HttpServer::new(move || App::new().app_data(Data::new(AppState { db: pool.clone() })))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        let bearer_middleware = HttpAuthentication::bearer(validator);
+        App::new()
+            .app_data(Data::new(AppState { db: pool.clone() }))
+            .service(basic_auth)
+            .service(create_user)
+            .service(
+                web::scope("")
+                    .wrap(bearer_middleware)
+                    .service(create_article),
+            )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
--- src/services.rs
+++ src/services.rs
@@ -13,11 +13,138 @@ use serde::{Deserialize, Serialize};
 use sha2::Sha256;
 use sqlx::{self, FromRow};
 
+#[derive(Deserialize)]
+struct CreateUserBody {
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, FromRow)]
+struct UserNoPassword {
+    id: i32,
+    username: String,
+}
+
+#[derive(Serialize, FromRow)]
+struct AuthUser {
+    id: i32,
+    username: String,
+    password: String,
+}
+
+#[derive(Deserialize)]
+struct CreateArticleBody {
+    title: String,
+    content: String,
+}
+
+#[derive(Serialize, FromRow)]
+struct Article {
+    id: i32,
+    title: String,
+    content: String,
+    published_by: i32,
+    published_on: Option<NaiveDateTime>,
+}
+
 #[post("/user")]
-async fn create_user(state: Data<AppState>) -> impl Responder {}
+async fn create_user(state: Data<AppState>, body: Json<CreateUserBody>) -> impl Responder {
+    let user: CreateUserBody = body.into_inner();
+
+    let hash_secret = std::env::var("HASH_SECRET").expect("HASH_SECRET must be set!");
+    let mut hasher = Hasher::default();
+    let hash = hasher
+        .with_password(user.password)
+        .with_secret_key(hash_secret)
+        .hash()
+        .unwrap();
+
+    match sqlx::query_as::<_, UserNoPassword>(
+        "INSERT INTO users (username, password)
+        VALUES ($1, $2)
+        RETURNING id, username",
+    )
+    .bind(user.username)
+    .bind(hash)
+    .fetch_one(&state.db)
+    .await
+    {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(error) => HttpResponse::InternalServerError().json(format!("{:?}", error)),
+    }
+}
 
 #[get("/auth")]
-async fn basic_auth(state: Data<AppState>) -> impl Responder {}
+async fn basic_auth(state: Data<AppState>, credentials: BasicAuth) -> impl Responder {
+    let jwt_secret: Hmac<Sha256> = Hmac::new_from_slice(
+        std::env::var("JWT_SECRET")
+            .expect("JWT_SECRET must be set!")
+            .as_bytes(),
+    )
+    .unwrap();
+    let username = credentials.user_id();
+    let password = credentials.password();
+
+    match password {
+        None => HttpResponse::Unauthorized().json("Must provide username and password"),
+        Some(pass) => {
+            match sqlx::query_as::<_, AuthUser>(
+                "SELECT id, username, password FROM users WHERE username = $1",
+            )
+            .bind(username.to_string())
+            .fetch_one(&state.db)
+            .await
+            {
+                Ok(user) => {
+                    let hash_secret =
+                        std::env::var("HASH_SECRET").expect("HASH_SECRET must be set!");
+                    let mut verifier = Verifier::default();
+                    let is_valid = verifier
+                        .with_hash(user.password)
+                        .with_password(pass)
+                        .with_secret_key(hash_secret)
+                        .verify()
+                        .unwrap();
+
+                    if is_valid {
+                        let claims = TokenClaims { id: user.id };
+                        let token_str = claims.sign_with_key(&jwt_secret).unwrap();
+                        HttpResponse::Ok().json(token_str)
+                    } else {
+                        HttpResponse::Unauthorized().json("Incorrect username or password")
+                    }
+                }
+                Err(error) => HttpResponse::InternalServerError().json(format!("{:?}", error)),
+            }
+        }
+    }
+}
 
 #[post("/article")]
-async fn create_article(state: Data<AppState>) -> impl Responder {}
+async fn create_article(
+    state: Data<AppState>,
+    req_user: Option<ReqData<TokenClaims>>,
+    body: Json<CreateArticleBody>,
+) -> impl Responder {
+    match req_user {
+        Some(user) => {
+            let article: CreateArticleBody = body.into_inner();
+
+            match sqlx::query_as::<_, Article>(
+                "INSERT INTO articles (title, content, published_by)
+                VALUES ($1, $2, $3)
+                RETURNING id, title, content, published_by, published_on",
+            )
+            .bind(article.title)
+            .bind(article.content)
+            .bind(user.id)
+            .fetch_one(&state.db)
+            .await
+            {
+                Ok(articles) => HttpResponse::Ok().json(articles),
+                Err(error) => HttpResponse::InternalServerError().json(format!("{:?}", error)),
+            }
+        }
+        _ => HttpResponse::Unauthorized().json("Unable to verify identity"),
+    }
+}
